4f9cbe4a67f9fd73e3da6f2ff8a415f19e5ec804
Tobie <khoichudang1@gmail.com>
2024-05-20 14:23:52 +0700

    newdatabase

--- porfolio2/settings.py
+++ porfolio2/settings.py
@@ -107,16 +107,16 @@ CKEDITOR_CONFIGS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Dang.khoi1',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'bXTbiNIzbzrZnTFnByEIpkwWUrvZFsQI',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '18539',
+    }
+}
 # DATABASES['default'] = dj_database_url.config(
 #     conn_max_age=600,
 #     conn_health_checks=True,
